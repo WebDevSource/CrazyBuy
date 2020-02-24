edf6961df3c7cafa6be9206ee0fa122610b4e7d2
jimmy <jimmy811114@gmail.com>
2020-02-24 22:30:23 +0800

    [20200224] 修正購物車

--- CrazyBuy/wwwroot/CrazyBuy/js/Cart.js
+++ CrazyBuy/wwwroot/CrazyBuy/js/Cart.js
@@ -1,5 +1,9 @@
 ﻿var cartApp = angular.module('CartApp', []).controller('CartCtrl', function ($scope) {
     Cart.InitView();
+
+    $scope.delCartItem = function (id) {
+        Cart.delCartItem(id);
+    };
 });
 
 var Cart = {
@@ -12,12 +16,30 @@ var Cart = {
         NavBar.Init();
     },
 
+    delCartItem(id) {
+        Utils.ProcessAjax("/api/ShopCart/"+id, "DELETE", true, "",
+            function (ret) {
+                if (ret.code === 1) {
+                    Cart.InitView();
+                } else {
+                    alert("service error");
+                }
+            },
+            function (error) { alert("ajax error") }
+        );
+    },
+
     InitView() {
         Utils.ProcessAjax("/api/ShopCart", "GET", true, "",
             function (ret) {
                 if (ret.code === 1) {
                     let appElement = document.querySelector('[ng-controller=CartCtrl]');
-                    let $scope = angular.element(appElement).scope();
+                    let $scope = angular.element(appElement).scope();                    
+
+                    $scope.TotalAmt = 0;
+                    $scope.CalculateSum = function (cart) {
+                        $scope.TotalAmt += cart.amount;
+                    }
 
                     $scope.carts = ret.data;
                     $scope.$apply();
