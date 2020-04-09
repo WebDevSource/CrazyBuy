a9ea2cec1d2b80383ff3295730d386d9f349c667
楊家碩 <jimmy811114@gmail.com>
2020-04-10 00:16:22 +0800

    [20200410] 新增公告頁面

--- CrazyBuy/wwwroot/CrazyBuy/js/Announcement.js
+++ CrazyBuy/wwwroot/CrazyBuy/js/Announcement.js
@@ -1,4 +1,8 @@
-﻿var Announcement = {
+﻿var cartApp = angular.module('AnnoApp', []).controller('AnnoCtrl', function ($scope) {
+
+});
+
+var Announcement = {
     doLoad() {
         Utils.Initial();
         Utils.InitI18next("zh-TW", "announcement", Announcement.InitModule);
@@ -10,9 +14,26 @@
         Announcement.InitView();
     },
 
-	InitView() {
-	
-    }
+    InitView() {
+        Announcement.getData();
+    },
+
+    getData() {
+        Utils.ProcessAjax("/api/tenant/getBulletin", "GET", true, "",
+            function (ret) {
+                if (ret.code === -1) {
+                    alert("service error");
+                } else {
+                    let appElement = document.querySelector('[ng-controller=AnnoCtrl]');
+                    let $scope = angular.element(appElement).scope();
+                    $scope.data = ret.data;
+                    $scope.$apply();
+                }
+            },
+            function (error) { alert("ajax error") }
+        );
+    },
+
 };
 
 window.onload = Announcement.doLoad;
