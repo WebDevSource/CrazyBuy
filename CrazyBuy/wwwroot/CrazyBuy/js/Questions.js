fc7ac9b812a8f24be4a3767aaf58ed1f34ae3c06
楊家碩 <jimmy811114@gmail.com>
2020-04-12 17:53:24 +0800

    [20200412] 新增首頁常見問題

--- CrazyBuy/wwwroot/CrazyBuy/js/Questions.js
+++ CrazyBuy/wwwroot/CrazyBuy/js/Questions.js
@@ -1,4 +1,10 @@
-﻿var Questions = {
+﻿var QuestionApp = angular.module('QuestionApp', []).controller('QuestionCtrl', function ($scope) {
+    $scope.action = function (key) {
+        $scope.value = $scope.map[key];
+    };
+});
+
+var Questions = {
     doLoad() {
         Utils.Initial();
         Utils.InitI18next("zh-TW", "questions", Questions.InitModule);
@@ -10,9 +16,50 @@
         Questions.InitView();
     },
 
-	InitView() {
-	
+    InitView() {
+        Questions.getFAQ();
+    },
+
+    getFAQ() {
+        Utils.ProcessAjax("/api/tenant/getFAQ", "GET", true, "",
+            function (ret) {
+                if (ret.code === -1) {
+                    alert("service error");
+                } else {
+                    let appElement = document.querySelector('[ng-controller=QuestionCtrl]');
+                    let $scope = angular.element(appElement).scope();
+
+                    let myMap = new Map();
+                    let data = ret.data;
+                    data.forEach(function (item, index, array) {
+                        let type = item.type;
+                        let list;
+                        if (myMap.has(type)) {
+                            list = myMap.get(type);
+                        } else {
+                            list = [];
+                        }
+                        list.push(item);
+                        myMap.set(type, list);
+                    });                    
+                    console.log(myMap);                    
+                    $scope.map = Questions.mapToData(myMap);
+                    $scope.value = $scope.map[data[0].type];
+                    $scope.$apply();
+                }
+            },
+            function (error) { alert("ajax error") }
+        );
+    },
+
+    mapToData(map) {
+        let result = {};
+        map.forEach(function (value, key) {
+            result[key] = value;
+        });
+        return result;
     }
+
 };
 
 window.onload = Questions.doLoad;
